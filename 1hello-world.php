5ee4c984afdfad0c3355995cac8c21fcd083b2ec
Nasir <131708693+nasir-siddiqui@users.noreply.github.com>
2025-05-08 00:08:46 +0530

    Add files via upload

---
+++ 1hello-world.php
@@ -0,0 +1,22 @@
+<?php
+    echo"Hello, I love Moon <br>";
+    echo"It's Shinning <br>";
+    //Comment
+    /*
+    This is a
+    multi-line comment
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Show Moon</button>
+    
+</body>
+</html>
---
+++ 2variable.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Variables - string, int, floats, booleans
+
+    $name = "John Cena"; //string
+    $age = 50; //int
+    $weight = 100.59; //floats
+
+    $food = "pizza";
+
+    $actor = true; //booleans (1 for true, 0 for false)
+
+    echo "Hello, $name is $age years old, he loves <strong>$food</strong>, weights $weight kg <br>
+    He is $actor <br>" ;
+
+    // echo"booleans (1 for true, 0 for false)";
+
+    echo"<br><br>";
+
+    $quantity = 3;
+    $price = 10;
+    $total = $quantity*$price;
+    echo"$name order $quantity of $food's, Total cost: {$total}\$"
+
+    //you can add variable like $price and {$price}
+?>
